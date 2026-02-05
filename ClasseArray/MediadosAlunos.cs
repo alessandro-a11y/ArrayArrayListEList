fd0f2846eb0e9e4b3e0e23708e2535a011f5c198
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-05 18:32:35 -0300

    Update MediadosAlunos.cs

--- ClasseArray/MediadosAlunos.cs
+++ ClasseArray/MediadosAlunos.cs
@@ -6,42 +6,71 @@ namespace ClasseArray
     {
         static void Main()
         {
-            string[] alunos = new string[5];
+            // criar arrays
+            string[] nomes = new string[5];
             double[] notas = new double[5];
 
-            // Coletando os nomes dos alunos
-            for (int i = 0; i < alunos.Length; i++)
+            for (int i = 0; i < nomes.Length; i++)
             {
-                Console.WriteLine($"Digite o nome do aluno {i + 1}: ");
-                alunos[i] = Console.ReadLine();
+                Console.WriteLine($"Informe o nome do aluno (id = {i}): ");
+                nomes[i] = Console.ReadLine();
             }
-
-            // Coletando as notas dos alunos
             for (int i = 0; i < notas.Length; i++)
             {
-                Console.WriteLine($"Digite a nota do aluno {i + 1}: ");
+                Console.WriteLine($"Informe a nota do aluno {nomes[i]}: ");
                 notas[i] = Convert.ToDouble(Console.ReadLine());
             }
-
-            // Calculando a soma das notas
+            foreach (double nota in notas)
+            {
+                Console.WriteLine($"Nota: {nota}");
+            }
+            foreach(string nome in nomes)
+            {
+                Console.WriteLine($"Nome: {nome}");
+            }
             double soma = 0;
             for (int i = 0; i < notas.Length; i++)
             {
                 soma += notas[i];
             }
-
-            // Calculando a média
             double media = soma / notas.Length;
-
-            // Exibindo os resultados
-            Console.WriteLine("\nRelatório dos alunos e suas notas:");
-            for (int i = 0; i < alunos.Length; i++)
-            {
-                Console.WriteLine($"Aluno: {alunos[i]} \t Nota: {notas[i]}");
-            }
-
-            // Exibindo a média final
-            Console.WriteLine($"\nMédia geral dos alunos: {media}");
+            Console.WriteLine($"Média da turma: {media}");
         }
     }
 }
+//string[] alunos = new string[5];
+//double[] notas = new double[5];
+
+//// Coletando os nomes dos alunos
+//for (int i = 0; i < alunos.Length; i++)
+//{
+//    Console.WriteLine($"Digite o nome do aluno {i + 1}: ");
+//    alunos[i] = Console.ReadLine();
+//}
+
+//// Coletando as notas dos alunos
+//for (int i = 0; i < notas.Length; i++)
+//{
+//    Console.WriteLine($"Digite a nota do aluno {i + 1}: ");
+//    notas[i] = Convert.ToDouble(Console.ReadLine());
+//}
+
+//// Calculando a soma das notas
+//double soma = 0;
+//for (int i = 0; i < notas.Length; i++)
+//{
+//    soma += notas[i];
+//}
+
+//// Calculando a média
+//double media = soma / notas.Length;
+
+//// Exibindo os resultados
+//Console.WriteLine("\nRelatório dos alunos e suas notas:");
+//for (int i = 0; i < alunos.Length; i++)
+//{
+//    Console.WriteLine($"Aluno: {alunos[i]} \t Nota: {notas[i]}");
+//}
+
+//// Exibindo a média final
+//Console.WriteLine($"\nMédia geral dos alunos: {media}");
