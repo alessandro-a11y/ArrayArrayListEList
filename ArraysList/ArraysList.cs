0d62aaf9b1b22a94b0f1290e6fd19881901f396d
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-09 10:05:38 -0300

    Create ArraysList.cs

---
+++ ArraysList/ArraysList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraysList
+{
+    internal class ArraysList
+    {
+        public static void Lista(string[] array, int index)
+        {
+            var lista = new ArrayList()
+            {
+                "Maria", 5, true, " ", null
+            };
+            lista.Insert(2, "Paulo");
+            lista.Add(3.5);
+            
+
+            foreach (var item2 in lista)
+            {
+                Console.WriteLine(item2);
+            }
+        }
+    }
+    
+}
