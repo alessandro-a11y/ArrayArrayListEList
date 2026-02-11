daa912c042390ba4ad0b38276685a7af11232da1
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-02-11 10:35:54 -0300

    Create Program.cs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+namespace Colecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+
+            
+            lista.Add("B");
+            lista.Add("A");
+            lista.Add("D");
+            lista.Add("C");          
+            lista.Add("F");
+            lista.Add("E");
+            Imprimir(lista);
+
+            lista.Sort();
+            Imprimir(lista);
+        }
+        static void Imprimir(List<string> lista)
+        {
+            foreach (var item in lista)
+            {
+                Console.Write($"{item} | ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
